37cec9af5ce93cd0da05a52513928bf55b70f3ac
Rick Ho <laekov.h@gmail.com>
2020-12-30 11:58:21 +0800

    fix model parallel in script

--- examples/pretrain_bert.sh
+++ examples/pretrain_bert.sh
@@ -3,8 +3,10 @@
 source /opt/spack/share/spack/setup-env.sh
 spack load python
 
-RANK=$OMPI_COMM_WORLD_RANK
-WORLD_SIZE=$OMPI_COMM_WORLD_SIZE
+export RANK=$OMPI_COMM_WORLD_RANK
+export WORLD_SIZE=$OMPI_COMM_WORLD_SIZE
+
+MP_SIZE=4
 
 if [ -z $RANK ]
 then
@@ -15,8 +17,9 @@ DATA_PATH=$HOME/Megatron-LM/data/my-bert_text_sentence
 CHECKPOINT_PATH=$HOME/Megatron-LM/ckpt/bert-$(date +%y%m%d-%H%M%S)
 
 python pretrain_bert.py \
+	   --model-parallel-size $MP_SIZE \
        --num-layers 6 \
-       --hidden-size 128 \
+       --hidden-size 512 \
        --num-attention-heads 8 \
        --batch-size 1 \
        --seq-length 128 \
